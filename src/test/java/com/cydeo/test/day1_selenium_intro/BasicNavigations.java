54bb8c02269322927f559c91fac0869a2e0194b3
Jennifer Radina <theradinas@gmail.com>
2022-09-15 13:46:34 -0500

    created webdriver

--- src/test/java/com/cydeo/test/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/test/day1_selenium_intro/BasicNavigations.java
@@ -1,6 +1,29 @@
 package com.cydeo.test.day1_selenium_intro;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BasicNavigations {
+    public static void main(String[] args) {
+
+        //1st step - setup Webdriver Manager
+        WebDriverManager.chromedriver().setup();
+
+        //2nd step - opening an empty driver - creating an instance of driver
+        WebDriver driver = new ChromeDriver();
+
+        //3 step -- navigate to page - go to https://
+        driver.get("https://www.tesla.com");
+
+
+
+
+
+
+
+
+    }
 
 
 }
